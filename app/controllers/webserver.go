cd2ade6a16f6f2bb3acabb1be226d10f9a94f34a
Tatsuya Yasuhara <tatsuya.yasuhara@sorich.jp>
2020-05-14 02:44:53 +0900

    Initial commit

---
+++ app/controllers/webserver.go
@@ -0,0 +1,140 @@
+package controllers
+
+import (
+	"encoding/json"
+	"flash-chart/config"
+	"fmt"
+	"html/template"
+	"log"
+	"math"
+	"net/http"
+	"strconv"
+)
+
+func StartWebServer() error {
+	port := config.Config.Port
+	http.HandleFunc("/calc/flash/", calcFlashHandler)
+	http.HandleFunc("/chart/", viewChartHandler)
+	log.Printf("start server at :%d, open on your browser -> http://localhost:%d/chart", port, port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+var templates = template.Must(template.ParseFiles("app/views/chart.html"))
+
+func viewChartHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "chart.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+type MyFloat float64
+
+type Temperature struct {
+	Values []float64 `json:"values,omitempty"`
+}
+
+type PowerDissipation struct {
+	Values []float64 `json:"values,omitempty"`
+}
+
+type DataFrameFlash struct {
+	Temperature Temperature `json:"temperature,omitempty"`
+	PowerDissipation PowerDissipation `json:"power_dissipation,omitempty"`
+}
+
+func calcFlashHandler(w http.ResponseWriter, r *http.Request) {
+	externalVoltage := r.URL.Query().Get("external_voltage")
+	if externalVoltage == "" {
+		log.Println("not fount: external_voltage")
+		return
+	}
+	ev, _ := strconv.ParseFloat(externalVoltage, 64)
+
+	vacancyEnergy := r.URL.Query().Get("vacancy_energy")
+	if vacancyEnergy == "" {
+		log.Println("not fount: vacancy_energy")
+		return
+	}
+	ve, _ := strconv.ParseFloat(vacancyEnergy, 64)
+
+	oxygenPressure := r.URL.Query().Get("oxygen_pressure")
+	if oxygenPressure == "" {
+		log.Println("not fount: oxygen_pressure")
+		return
+	}
+	pO2, _ := strconv.ParseFloat(oxygenPressure, 64)
+
+	//log.Printf("ev=%g, ve=%g\n", ev, ve)
+
+	dff := CalcW(ev, ve, pO2)
+
+	js, err := json.Marshal(dff)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+const NDP int = 6001
+const RR float64 = 8.3145
+const FF float64 = 96485.33289
+var nd = NDP - 1
+var Cp = 56.188
+var vm0 = 123.233e-03/(6.10e+03)
+var Cpv = Cp/vm0
+var e0 = 1.6021766208e-19
+var N0 = 7.0e+20
+var B0 = math.Pow(2.0 * math.Exp(RR/RR), 1.0/3.0)
+var MQ0 = 1.89*FF
+var MM0 = 8.02e-02
+var zp = -7.5
+var ext_pote = zp*FF
+var dt = 1.0
+var dTFdt = 1.0/6.0
+var ds = 0.01*dt
+
+func CalcW(voltage, vacancyE, pressure float64) *DataFrameFlash {
+	//nd := NDP - 1
+	//Cp := 56.188
+	//vm0 := 123.233e-03/(6.10e+03)
+	//Cp := Cp/vm0
+	//e0 := 1.6021766208e-19
+	//N0 := 7.0e+20
+	//B0 := math.Pow(2.0 * math.Exp(RR/RR), 1.0/3.0)
+	//MQ0 := 1.89*FF
+	//MM0 := 8.02e-02
+	//zp := -7.5
+	//ext_pote := zp*FF
+	//dt := 1.0
+	//dTFdt := 1.0/6.0
+	//ds := 0.01*dt
+	// ----------------------
+	TF := 773.0
+	T := TF
+	// 外部入力3項
+	vol := voltage
+	dHv := vacancyE * FF
+	pO2 := pressure
+	var tList []float64
+	var wList []float64
+	for ii := 0; ii <= nd; ii++ {
+		TF += dTFdt * dt
+		tList = append(tList, TF - 273) //todo 初回のみの処理にしたい
+		for i := 1; i <= 100; i++ {
+			n1 := N0 * B0 * math.Pow(pO2, -1.0/6.0) * math.Exp(-(dHv+ext_pote)/3./RR/T)
+			dTdt := vol * vol * e0/Cpv * n1 * MM0 * math.Exp(-MQ0/RR/T)
+			T += dTdt*ds
+		}
+		if T <= TF { T = TF }
+		n1 := N0 * B0 * math.Pow(pO2, -1.0/6.0) * math.Exp(-(dHv+ext_pote)/3./RR/T)
+		I := vol * e0 * n1 * MM0 * math.Exp(-MQ0/RR/T)
+		W := I * vol
+		wList = append(wList, W)
+	}
+	return &DataFrameFlash{
+		Temperature:      Temperature{tList},
+		PowerDissipation: PowerDissipation{wList},
+	}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"flash-chart/app/controllers"
+	"log"
+)
+
+func main() {
+	log.Println(controllers.StartWebServer())
+}
